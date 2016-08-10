7057f6b84df3d6e821e7a4c176392da0ce6dbcb4
samuelokrent <samuelokrent@gmail.com>
2016-08-09 21:31:16 -0500

    newfile

--- 3/example_1.js
+++ 3/example_1.js
@@ -1,3 +1,13 @@
 function doStuff(arg) {
 	// Do some stuff here
+	var i = 1;
+	while(i < 10) {
+		// stuff...
+		// more stuff...
+		i = i + 1;
+	}
+
+	for(var i = 1; i < 4; i = i + 1) {
+		alert("i is now: " + i);
+	}
 }	
