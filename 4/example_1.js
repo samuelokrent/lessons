2bde89f00c35e694283a2c05ef5337901fc65a81
samuelokrent <samuelokrent@gmail.com>
2016-08-31 19:23:58 -0500

    lesson 5

--- 4/example_1.js
+++ 4/example_1.js
@@ -1,7 +1,10 @@
 function doStuff(arg) {
 
-	for(var i = 0; i < arg; i = i + 1) {
-		alert("i is now: " + i);
+	for(var i = 2; i < arg; i = i + 1) {
+		console.log ("Sam says i is now: " + i);
 	}
 
 }	
+
+var hi = [1,2,4,7];
+hi[0]
